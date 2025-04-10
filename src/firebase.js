d8fd9bd3813d1aabc192b66cc6cfd94110009ae9
pavanpatel18 <pjp22@njit.edu>
2025-04-10 09:18:51 -0400

    Add Firebase config with .env setup

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.AIzaSyCLirwSHuq_3ffyBp-l8mtCsE6jH9OS0tw,
-  authDomain: process.env.chat-app-302b9.firebaseapp.com,
-  projectId: process.env.chat-app-302b9,
-  storageBucket: process.env.chat-app-302b9.firebasestorage.app,
-  messagingSenderId: process.env.783695018343,
-  appId: process.env.1:783695018343:web:276b362435dbab4d27d7c2,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
